df3315b2981677f49367358fc9048d207de4906b
Imamsubekti26 <imam0subekti@gmail.com>
2024-01-30 00:34:07 +0700

    create database logger

--- utils/database/connection.go
+++ utils/database/connection.go
@@ -2,11 +2,14 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	xlogger "github.com/Imamsubekti26/Perpustakaan_Go/utils/XLogger"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type dbInstance struct {
@@ -22,7 +25,12 @@ func Connection() (*dbInstance, error) {
 		os.Getenv("DB_NAME"),
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	gormConfig, err := dbLogger()
+	if err != nil {
+		return nil, xlogger.Error(err)
+	}
+
+	db, err := gorm.Open(mysql.Open(dsn), gormConfig)
 	if err != nil {
 		return nil, xlogger.Error(err)
 	}
@@ -30,4 +38,25 @@ func Connection() (*dbInstance, error) {
 	var dbInstance dbInstance
 	dbInstance.Connection = db
 	return &dbInstance, nil
+}
+
+func dbLogger() (*gorm.Config, error) {
+	today := time.Now().Format("2006-01-02")
+	filePath := fmt.Sprintf("logs/%s-db.log", today)
+	logFile, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		return nil, xlogger.Error(err)
+	}
+	logWriter := log.New(logFile, "\r\n", log.LstdFlags)
+
+	return &gorm.Config{
+		Logger: logger.New(
+			logWriter,
+			logger.Config{
+				SlowThreshold: time.Second,
+				LogLevel:      logger.Info,
+				Colorful:      true,
+			},
+		),
+	}, nil
 }
